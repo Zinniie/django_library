2aec143c0bf899ae5d2b548e11c95150f983a657
Zinniie <ezinne.blessing@yahoo.com>
2022-07-16 04:59:56 +0100

    lib

--- API/urls.py
+++ API/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import BookApiView
+from .views import BookApiView, BookApiDetail, BookApiCreate, BookApiUpdate, BookApiDelete
 
 urlpatterns = [
     path('', BookApiView.as_view(), name='home'),
+    path('<int:pk>/', BookApiDetail.as_view()),
+    path('create', BookApiCreate.as_view()),
+    path('update/<int:pk>/', BookApiUpdate.as_view()),
+    path('delete/<int:pk>/', BookApiDelete.as_view()),
 ]
--- API/views.py
+++ API/views.py
@@ -12,6 +12,24 @@ class BookApiView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
+class BookApiCreate(generics.CreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class BookApiDetail(generics.RetrieveAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class BookApiUpdate(generics.UpdateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class BookApiDelete(generics.DestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+
 
 
     
